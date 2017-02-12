1fbf6417cdb5dece6b31a0e019e9540377b3cf9f
midorigoke <midorigoke@desumachi.tk>
2017-02-12 20:57:02 +0900

    プロジェクト ファイルを追加します。

---
+++ get_youtubelive_comments_cui/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_youtubelive_comments_cui
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
